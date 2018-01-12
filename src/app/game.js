88920d160512fc8b735cb6ff032cb6b3bc0ab824
rendfall <rendfall@gmail.com>
2018-01-12 01:59:35 +0100

    Create player + handle input events

--- src/app/game.js
+++ src/app/game.js
@@ -1,16 +1,27 @@
 import { AudioChannel , GameLoop} from './engine';
 import { Keyboard } from './engine/keyboard';
 import { Sprite } from './engine/sprite';
+import { APP_CONFIG } from './config';
 
 export class Entity {
+    game = null;
+    name = null;
     body = null;
     width = 25;
     height = 100;
+    x = 0;
+    y = 0;
+    moveBy = 5;
 
-    constructor(name) {
+    constructor(game, name) {
+        this.game = game;
+        this.name = name;
     }
 
-    createBody() {
+    createBody(x, y) {
+        this.x = x;
+        this.y = y;
+
         return new Promise((resolve) => {
             const canvas = document.createElement('canvas');
             canvas.width = this.width;
@@ -27,11 +38,36 @@ export class Entity {
 
             image.src = canvas.toDataURL();
             this.body = image;
-        })
+        });
+    }
+
+    moveUp(isPressed) {
+        this.y -= this.moveBy;
+    }
+
+    moveDown(isPressed) {
+        this.y += this.moveBy;
+    }
+
+    setupInput(keyMapping) {
+        const input = this.game.keyboard;
+        const { up, down } = keyMapping;
+
+        input.onKey(up, (isPressed) => {
+            this.moveUp(isPressed);
+        });
+        input.onKey(down, (isPressed) => {
+            this.moveDown(isPressed);
+        });
+    }
+
+    draw(context, deltaTime) {
+        context.drawImage(this.body, this.x, this.y);
     }
 }
 
 export class Game {
+    context = null;
     loop = new GameLoop();
     keyboard = new Keyboard();
     audioChannel = new AudioChannel();
@@ -43,10 +79,10 @@ export class Game {
     constructor() {
         console.log('The game is ready.');
 
+        this.context = window.app.canvas.getContext('2d');
         this.setupKeyboard();
         this.setupAudio();
         this.setupSprites();
-        this.setupLoop();
     }
 
     setupKeyboard() {
@@ -56,17 +92,13 @@ export class Game {
     setupAudio() {}
 
     setupSprites() {
-        this.playerOne = new Entity('player-one');
+        this.playerOne = new Entity(this, 'player-one');
+        this.playerOne.setupInput({ up: 'w', right: 'd', down: 's', left: 'a' });
 
-        this.playerOne.createBody()
+        this.playerOne.createBody(0, 0)
             .then((image) => {
-                this.draw(image, 0, 50)
+                this.setupLoop();
             });
-    }
-
-    draw(image, x, y) {
-        const context = window.app.canvas.getContext('2d');
-        context.drawImage(image, x, y);
     }
 
     setupLoop() {
@@ -75,7 +107,15 @@ export class Game {
         this.loop.start();
     }
 
+    refreshContext() {
+        this.context.clearRect(0, 0, APP_CONFIG.width, APP_CONFIG.height);
+    }
+
     update(deltaTime) {}
 
-    redraw(deltaTime) {}
+    redraw(deltaTime) {
+        this.refreshContext();
+
+        this.playerOne.draw(this.context, deltaTime);
+    }
 }
